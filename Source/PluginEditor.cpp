bba4717a0b333b6e43e4d11902d5bf0c9c0a5032
arcathrax <arcathrax@proton.me>
2025-03-27 16:28:15 +0100

    fix: remove default "Hello World" screen

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -5,8 +5,6 @@
 ArcaGainAudioProcessorEditor::ArcaGainAudioProcessorEditor (ArcaGainAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
     setSize (400, 300);
 }
 
@@ -17,16 +15,9 @@ ArcaGainAudioProcessorEditor::~ArcaGainAudioProcessorEditor()
 
 void ArcaGainAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void ArcaGainAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -15,8 +15,6 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
     ArcaGainAudioProcessor& audioProcessor;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ArcaGainAudioProcessorEditor)
